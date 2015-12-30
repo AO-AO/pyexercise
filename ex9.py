b8042eac6c67c5d1e6fda7e9d0318ea4ba3f8ea9
Xu Ao <583252976@qq.com>
2015-12-30 17:23:36 +0800

    replace str

---
+++ ex9.py
@@ -0,0 +1,24 @@
+"""
+Write a function called censor that takes two strings, 
+text and word, 
+as input. 
+It should return the text with the word you chose replaced with asterisks.
+"""
+
+def censor(text, word):
+    lenth_wd = len(word)
+    rpl_wd = "*" * lenth_wd
+    result = ""
+    i = 0
+    while i + lenth_wd <= len(text):
+        if text[i : i + lenth_wd] == word:
+            result = result + rpl_wd
+            i = i + lenth_wd
+        else:
+            result = result + text[i]
+            i = i + 1
+    return result
+
+
+result = censor("this hack is wack hack", "hack")
+print result
